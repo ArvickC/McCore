cb48ec4a4805f645c40eb32af997bcda2841d073
ArvickC <arvick@outlook.com>
2020-10-26 23:21:45 -0700

    First Commit

---
+++ src/main/java/me/crazybanana/mccore/McCore.java
@@ -0,0 +1,17 @@
+package me.crazybanana.mccore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class McCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
